136dbd8fc00ed830eedd4b38ee69fc378ec96973
gabrielaaferreira <gabiferreirarj@outlook.com>
2024-07-08 12:42:24 -0300

    Sincronização de todo repositório

---
+++ ONE_literalura/src/main/java/com/oneoracle/literalura/AutorRepository.java
@@ -0,0 +1,11 @@
+package com.oneoracle.literalura;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AutorRepository extends JpaRepository<Autor, Long> {
+    Optional<Autor> findByNome(String nome);
+}
